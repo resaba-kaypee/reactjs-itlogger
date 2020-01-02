addfd4013380ec1d4347cb4bbf60ee284d33f5ee
kaypee <resaba.kaypee@gmail.com>
2020-01-03 01:53:55 +0700

    Delete Log

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,10 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from "./types";
 
 // Get logs from server
 export const getLogs = () => async dispatch => {
   try {
     setLoading();
-    const res = await fetch("/logs");
+    const res = await fetch("/logs?_sort=date&_order=desc");
     const data = await res.json();
 
     dispatch({
@@ -35,6 +35,26 @@ export const addLog = (log) => async dispatch => {
     dispatch({
       type: ADD_LOG,
       payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+// Delete log from the server
+export const deleteLog = (id) => async dispatch => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`,{
+      method: 'DELETE'
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id
     });
   } catch (err) {
     dispatch({
