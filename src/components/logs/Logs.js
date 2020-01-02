d889df87bcf6e5aabe0533f2f44504e974d910e8
kaypee <resaba.kaypee@gmail.com>
2020-01-03 00:45:58 +0700

    Connecting redux to a Component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,29 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getLogs } from "../../actions/logActions";
+import PropTypes from "prop-types";
 import LogItem from "./LogItem";
-import Preloader from "../layout/Preloader"
+import Preloader from "../layout/Preloader";
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
     // eslint-disavle-next-line
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
-    return <Preloader/>;
+  if (loading || logs === null) {
+    return <Preloader />;
   }
-  
+
   return (
     <ul className="collection with-header">
       <li className="collection-header">
@@ -32,10 +23,18 @@ const Logs = () => {
       {!loading && logs.length === 0 ? (
         <p className="center">No logs to show...</p>
       ) : (
-        logs.map(log => <LogItem key={log.id} log={log}/>)
+        logs.map(log => <LogItem key={log.id} log={log} />)
       )}
     </ul>
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  log: state.log
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
