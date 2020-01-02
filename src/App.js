79714d52c01922dda8e455f65c148addbed04e19
kaypee <resaba.kaypee@gmail.com>
2020-01-02 23:50:16 +0700

    Redux Store Provider setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, {useEffect, Fragment} from 'react';
+import React, { useEffect, Fragment } from "react";
 import SearchBar from "./components/layout/Searchbar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
-import AddLogModal from "./components/logs/AddLogModal"
-import EditLogModal from "./components/logs/EditLogModal"
-import AddTechModal from "./components/techs/AddTechModal"
-import TechListModal from "./components/techs/TechListModal"
-import 'materialize-css/dist/css/materialize.min.css'
-import M from 'materialize-css/dist/js/materialize.min.js'
-import './App.css';
+import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
 
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     // Init Materialize js
     M.AutoInit();
-  })
+  });
 
   return (
+    <Provider store={store}>
     <Fragment>
-      <SearchBar/>
+      <SearchBar />
       <div className="container">
-        <AddBtn/>
-        <AddLogModal/>
-        <EditLogModal/>
-        <AddTechModal/>
-        <TechListModal/>
-        <Logs/>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
       </div>
     </Fragment>
+    </Provider>
   );
-}
+};
 
 export default App;
