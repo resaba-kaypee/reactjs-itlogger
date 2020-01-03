9bcfd98e0918e222b09571f2530e88eb1e264980
kaypee <resaba.kaypee@gmail.com>
2020-01-04 05:27:07 +0700

    add delete tech from db

--- routes/techs.js
+++ routes/techs.js
@@ -1,25 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const Tech = require("../models/Tech");
 
 // @route POST api/techs
 // @dec Add a tech
 // @access public
-router.post("/", (req, res) => {
-  res.send("Register a tech");
-});
+router.post(
+  "/",
+  [
+    check("firstName", "First Name is required")
+      .not()
+      .isEmpty(),
+    check("lastName", "Last Name is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { firstName, lastName } = req.body;
+    try {
+      let tech = new Tech({
+        firstName,
+        lastName
+      });
+      await tech.save();
+      res.json(tech)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route GET api/techs
 // @dec Get all tech
 // @access public
-router.get("/", (req, res) => {
-  res.send("Get all techs");
+router.get("/", async (req, res) => {
+  try {
+    const techs = await Tech.find({});
+    res.json(techs);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
 });
 
 // @route DELETE api/techs/:id
 // @dec Delete a tech
 // @access public
-router.delete("/:id", (req, res) => {
-  res.send("Delete a tech");
+router.delete("/:id", async (req, res) => {
+  await Tech.findByIdAndRemove(req.params.id);
+  res.json({ mgs: "Tech removed" });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+// connect to db
+connectDB();
+
+// init middleware
+app.use(express.json({ extented: false }));
+
 // define routes
-app.use('/api/logs', require('./routes/logs'))
-app.use('/api/techs', require('./routes/techs'))
+app.use("/api/logs", require("./routes/logs"));
+app.use("/api/techs", require("./routes/techs"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
