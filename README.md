60a8e92f2525561cb507df7928580c29ebd73413
kaypee <resaba.kaypee@gmail.com>
2020-01-04 14:51:43 +0700

    add log to db

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-### Small IT logger App
-# Uses json-server a mock server
-# Redux for state management
-# Material UI for design
+# Small IT logger App
+### Uses json-server a mock server
+#### Redux for state management
+#### Material UI for design
 
-### Avalable scripts
-# `npm run dev`
+# Avalable scripts
+#### `npm run dev`
 Using concurrently package it will run both the server at port 5000 and client side at port 3000 in development mode
+
+# todos
+* protect api routes
--- routes/logs.js
+++ routes/logs.js
@@ -1,12 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+
+const Log = require("../models/Log");
 
 // @route POST api/logs
 // @dec Add a log
 // @access public
-router.post("/", (req, res) => {
-  res.send("Add a log");
-});
+router.post(
+  "/",
+  [
+    check("message", "Message is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array()})
+    }
+
+    const {message, attention, tech, date} = req.body;
+    try {
+      const newLog = new Log({
+        message, attention, tech, date
+      })
+      await newLog.save();
+      res.json(newLog);
+    } catch (err) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 // @route GET api/logs
 // @dec Get all logs
